beb32798de1b9fb91ae5e1beffade78efa18923b
Dimitris Mantzouranis <d3xter93@gmail.com>
2022-01-28 16:34:20 +0200

    usb: init the buffer use hw defaults

--- os/hal/ports/SN32/LLD/SN32F2xx/USB/hal_usb_lld.c
+++ os/hal/ports/SN32/LLD/SN32F2xx/USB/hal_usb_lld.c
@@ -517,6 +517,9 @@ void usb_lld_stop(USBDriver *usbp) {
 void usb_lld_reset(USBDriver *usbp) {
 
   /* Post reset initialization.*/
+  for(int i=1;i <= USB_ENDPOINTS_NUMBER; i++){
+    SN32_USB->EPBUFOS[i-1] = (0x40 * i);
+  }
   SN32_USB->INSTSC = (0xFFFFFFFF);
   SN32_USB->ADDR  = 0;
   usb_lld_stall_out(usbp, 0);
