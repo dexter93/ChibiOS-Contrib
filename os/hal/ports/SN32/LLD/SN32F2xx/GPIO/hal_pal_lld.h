b6893f3ca7a74de489f8b9683765215be01743d6
Dimitris Mantzouranis <d3xter93@gmail.com>
2023-05-11 14:41:56 +0300

    sn32 pal: fix pal_lld_writepad
    We need to set the logical HIGH/LOW state here
    NOT mess with DATA

--- os/hal/ports/SN32/LLD/SN32F2xx/GPIO/hal_pal_lld.h
+++ os/hal/ports/SN32/LLD/SN32F2xx/GPIO/hal_pal_lld.h
@@ -391,7 +391,10 @@ typedef uint32_t iopadid_t;
  *
  * @notapi
  */
-#define pal_lld_writepad(port, pad, bit) ((port)->DATA = (bit << pad))
+#define pal_lld_writepad(port, pad, bit) do {    \
+    if(bit > PAL_LOW) pal_lld_setpad(port, pad); \
+    else pal_lld_clearpad(port, pad);            \
+} while(0)
 
 /**
  * @brief   Sets a pad logical state to @p PAL_HIGH.
